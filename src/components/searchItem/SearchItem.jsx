69f832ed70708265e2d525ac9087d5a69a172e8f
dramnay <deepanshu.ramnay@gmail.com>
2023-07-17 19:21:44 +0530

    hotels add

--- src/components/searchItem/SearchItem.jsx
+++ src/components/searchItem/SearchItem.jsx
@@ -3,14 +3,9 @@ import "./searchItem.css";
 const SearchItem = () => {
     return (
         <div className="searchItem">
-            <img
-                src="https://cf.bstatic.com/xdata/images/hotel/square600/287476409.webp?k=566be1be0d8e73c488de449e460bb7d9abdb19c84dfa30513c32dc6a9c764272&o="
-                alt=""
-                className="siImg"
-            />
             <div className="siDesc">
-                <h1 className="siTitle">Hyatt palace Jaipur</h1>
-                <span className="siDistance">500m from center</span>
+                <h1 className="siTitle">AbC</h1>
+                <span className="siDistance">200m</span>
                 <span className="siSubtitle">Double Deluxe Room</span>
                 <span className="siFeatures">Multiple Bed Types</span>
                 <span className="siCancelOp">Free cancellation </span>
@@ -24,7 +19,7 @@ const SearchItem = () => {
                     <button>9.4</button>
                 </div>
                 <div className="siDetailTexts">
-                    <span className="siPrice">Rs5000</span>
+                    <span className="siPrice">Rs4000</span>
                     <span className="siTaxOp">Includes taxes and fees</span>
                     <button className="siCheckButton">See availability</button>
                 </div>
